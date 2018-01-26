8750eb43942d9cb6bc8bcc56a2bdcc8971e08f7e
CoolCatCoder <marialenakon84@gmail.com>
2018-01-26 12:58:01 +0100

    Second commit

--- src/main/java/com/salvo/salvo/GameRepository.java
+++ src/main/java/com/salvo/salvo/GameRepository.java
@@ -4,9 +4,11 @@ package com.salvo.salvo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 // GameRepository is defined to be a JpaRepository that manages instances of Games
 
 @RepositoryRestResource   // A Repository class is analogous to a table
 public interface GameRepository extends JpaRepository<Game, Long> {
-
+       // List<Game> findById <Long id>
 }
--- src/main/java/com/salvo/salvo/SalvoController.java
+++ src/main/java/com/salvo/salvo/SalvoController.java
@@ -1,4 +1,35 @@
 package com.salvo.salvo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import java.util.Set;
+
+import static java.util.stream.Collectors.toList;
+
+@RestController
+@RequestMapping("/api")  // adds /api to all URLs for this controller, so that they are not confused with the /rest
 public class SalvoController {
+
+    @Autowired
+    private GameRepository repo;
+
+    @RequestMapping("/games")
+    public List<Object> getGameIds() {
+       return repo.findAll().stream()
+                .map(b -> b.getId())
+                .collect(toList());
+    }
+
 }
+//    List<Object> getGameIds(Set<Game> allGames) {
+//        return allGames.stream().map(Game::getId).collect(Collectors::toList);
+//    }
+
+//public Set<BillingType> getBillingTypes(List<Billing> billings) {
+//  return
+//    billings.stream()
+//            .map(b -> b.getType())
+//            .collect(toSet());
+//}
